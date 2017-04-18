24db26cbdfb5612414826b96b50d3eaf53c9d938
Michael Chapman <submariner100@gmail.com>
2017-04-18 13:58:50 +0100

    first commit

---
+++ Project12/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Macbook on 13/04/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     let defaults = UserDefaults.standard
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+
+
+
+
+     }
+
+     
+
+}
+
